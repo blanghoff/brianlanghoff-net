2bc3fccb9db250899f917d70cfab59f654649fe8
Brian Langhoff <brian@intelligent.education>
2019-03-20 14:26:14 -0500

    add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'TechElement',
-    siteDescription: 'TechElement - Comments on Technology and Humanity',
+    title: 'BrianLanghoff.net',
+    siteDescription: 'BrianLanghoff.net - Comments on Technology and Humanity',
     authorName: 'Brian Langhoff'
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'TechElement',
-        short_name: 'TechElement',
+        name: 'BrianLanghoff.net',
+        short_name: 'BrianLanghoff.net',
         start_url: '/',
         background_color: '#6b37bf',
         theme_color: '#6b37bf',
@@ -18,6 +18,30 @@ module.exports = {
         display: 'standalone',
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-136622911-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
